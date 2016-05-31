9f2abfb94359e5af0702f5c90c8003df186ddd73
quke <quke@baidu.com>
2016-05-31 11:34:15 +0800

    add 修改密码

---
+++ HRER/LoginController/HRNewPassViewController.h
@@ -0,0 +1,13 @@
+//
+//  HRNewPassViewController.h
+//  HRER
+//
+//  Created by quke on 16/5/31.
+//  Copyright © 2016年 linjunhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRNewPassViewController : UIViewController
+
+@end
---
+++ HRER/LoginController/HRNewPassViewController.m
@@ -0,0 +1,25 @@
+//
+//  HRNewPassViewController.m
+//  HRER
+//
+//  Created by quke on 16/5/31.
+//  Copyright © 2016年 linjunhou. All rights reserved.
+//
+
+#import "HRNewPassViewController.h"
+
+@interface HRNewPassViewController ()
+
+@end
+
+@implementation HRNewPassViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+
+@end
