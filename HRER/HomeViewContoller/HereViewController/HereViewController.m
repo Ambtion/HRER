facbf787d9205e8c875ed04ab176e21c4ba7750b
Qu Ke <kequ@sohu-inc.com>
2016-06-08 15:13:45 +0800

    添加主页

--- HRER/HomeViewContoller/HereViewController/HereViewController.m
+++ HRER/HomeViewContoller/HereViewController/HereViewController.m
@@ -7,13 +7,62 @@
 //
 
 #import "HereViewController.h"
+#import "RefreshTableView.h"
+
+@interface HereViewController()<UITableViewDataSource,UITableViewDelegate>
+
+@property(nonatomic,strong)RefreshTableView * tableView;
+@property(nonatomic,strong)NSMutableArray * dataSource;
+@end
 
 @implementation HereViewController
 
+#pragma mark - ViewLife
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+
+#pragma mark - Init
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
+    [self initUI];
 }
+
+- (void)initUI
+{
+    self.tableView = [[RefreshTableView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height) style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+    
+    [self initRefreshView];
+}
+
+- (void)initRefreshView
+{
+    WS(ws);
+    self.tableView.refreshHeader.beginRefreshingBlock = ^(){
+        
+    };
+    
+    self.tableView.refreshFooter.beginRefreshingBlock = ^(){
+    
+    };
+}
+
+#pragma mark - TableViewDelegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSource.count;
+}
+
 
 @end
