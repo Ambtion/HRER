817c49b137e1966d77d913588bc359a77fe64da4
Qu Ke <kequ@sohu-inc.com>
2016-07-21 20:51:31 +0800

    add upload

---
+++ HRER/HRPOICrete/HRUploadImageView.h
@@ -0,0 +1,13 @@
+//
+//  HRUploadImageView.h
+//  HRER
+//
+//  Created by kequ on 16/7/20.
+//  Copyright © 2016年 linjunhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRUploadImageView : UIView
+
+@end
---
+++ HRER/HRPOICrete/HRUploadImageView.m
@@ -0,0 +1,13 @@
+//
+//  HRUploadImageView.m
+//  HRER
+//
+//  Created by kequ on 16/7/20.
+//  Copyright © 2016年 linjunhou. All rights reserved.
+//
+
+#import "HRUploadImageView.h"
+
+@implementation HRUploadImageView
+
+@end
