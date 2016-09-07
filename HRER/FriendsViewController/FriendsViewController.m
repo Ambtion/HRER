55d94cfdf8f9349843c29d6161c7b0c5cf1d5b73
quke <quke@baidu.com>
2016-09-07 17:06:54 +0800

    修改分享文案

--- HRER/FriendsViewController/FriendsViewController.m
+++ HRER/FriendsViewController/FriendsViewController.m
@@ -394,7 +394,7 @@
 {
     NSString * userId = [[LoginStateManager getInstance] userLoginInfo].user_id;
     NSString * url = [NSString stringWithFormat:@"http://58.135.93.4:8089/sharePage?id=%@",userId];
-    [[HRQQManager shareInstance] shareNewsWithImage:[UIImage imageNamed:@"add"] title:@"这里" Des:@"这里不错" link:url WithCallBack:^(QQBaseResp *response) {
+    [[HRQQManager shareInstance] shareNewsWithImage:[UIImage imageNamed:@"add"] title:@"邀请你来这里" Des:@"这里是你和好朋友的旅行朋友圈\n在这里朋友越多，出门靠谱的选择越多" link:url WithCallBack:^(QQBaseResp *response) {
     }];
 }
 
@@ -402,7 +402,7 @@
 {
     NSString * userId = [[LoginStateManager getInstance] userLoginInfo].user_id;
     NSString * url = [NSString stringWithFormat:@"http://58.135.93.4:8089/sharePage?id=%@",userId];
-    [HRWebCatShare sendWeixinWebContentTitle:@"这里" description:@"这里不错"  thumbImage:[UIImage imageNamed:@"add"] webpageURL:url scene:WXSceneSession withcallBack:^(BaseResp *resp) {
+    [HRWebCatShare sendWeixinWebContentTitle:@"邀请你来这里" description:@"这里是你和好朋友的旅行朋友圈\n在这里朋友越多，出门靠谱的选择越多"  thumbImage:[UIImage imageNamed:@"add"] webpageURL:url scene:WXSceneSession withcallBack:^(BaseResp *resp) {
         
     }];
 }
